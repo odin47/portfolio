450ea5a5339e9c8026616f752d1cc4418ad9803f
Deepak PD <pddeepak599@gmail.com>
2020-01-29 18:22:56 +0530

    Contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
 import Education from '../components/Education';
+import Contact from '../components/Contact';
 
 const index = () => {
     return (
@@ -14,6 +15,7 @@ const index = () => {
             <Hero />
             <Skills />
             <Education />
+            <Contact />
         </Layout>
     )
 }
